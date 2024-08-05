d23ae330abac123edc879021512317ba9a801f90
DianeLarsen <diane.larsen@outlook.com>
2024-08-05 12:01:56 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -33,6 +33,18 @@ const pool = new Pool({
     await app.prepare();
 
     const server = express();
+    server.use((req, res, next) => {
+      const hostname = req.hostname === 'www.app.domain.com' ? 'app.domain.com' : req.hostname;
+
+      if (req.headers['x-forwarded-proto'] === 'http' || req.hostname === 'www.app.domain.com') {
+        res.redirect(301, `https://${hostname}${req.url}`);
+        return;
+      }
+
+      res.setHeader('strict-transport-security', 'max-age=31536000; includeSubDomains; preload');
+      next();
+    });
+
 
     server.all('*', (req, res) => {
       return handle(req, res);
