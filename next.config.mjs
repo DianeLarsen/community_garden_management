f1d806bbd0f72e2e798bf874c88b399d801a3210
DianeLarsen <diane.larsen@outlook.com>
2024-08-05 08:37:14 -0700

    Prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,65 @@
 /** @type {import('next').NextConfig} */
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' res.cloudinary.com media.gettyimages.com;
+  connect-src 'self';
+  font-src 'self';
+  frame-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'geolocation=(), microphone=(), camera=()',
+  },
+];
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "media.gettyimages.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'media.gettyimages.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
       },
     ],
   },
-  
 };
 
 export default nextConfig;
