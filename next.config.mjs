0f1ea654ceebc661635b08d9f99537bc74249cf7
DianeLarsen <diane.larsen@outlook.com>
2024-08-07 08:50:26 -0700

    Prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,13 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: `
     default-src 'self';
-    script-src 'nonce-{script value}' 'strict-dynamic' https: 'unsafe-eval' blob: https://upload-widget.cloudinary.com https://maps.googleapis.com https://*.googleapis.com;
-    style-src 'self' https://fonts.googleapis.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://upload-widget.cloudinary.com https://maps.googleapis.com https://*.googleapis.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     img-src 'self' https://res.cloudinary.com https://*.googleapis.com https://*.gstatic.com data:;
     connect-src 'self' https://maps.googleapis.com https://*.googleapis.com https://*.gstatic.com;
     font-src data: https://fonts.gstatic.com;
     frame-src https://*.google.com;
+    worker-src blob:;
   `.replace(/\n/g, ' '),
   },
   {
