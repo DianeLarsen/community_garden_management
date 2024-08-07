a677200f3738e52d594a893f4604c35b0a072fad
DianeLarsen <diane.larsen@outlook.com>
2024-08-07 07:45:05 -0700

    Prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,41 +2,32 @@
 
 const securityHeaders = [
   {
-    key: "Content-Security-Policy",
-    value: `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline' https://upload-widget.cloudinary.com https://maps.googleapis.com;
-    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' https://res.cloudinary.com https://maps.gstatic.com;
-    connect-src 'self' https://maps.googleapis.com;
-    font-src 'self' https://fonts.gstatic.com;
-    object-src 'none';
-    frame-ancestors 'none';
-  `,
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; img-src 'self' https://res.cloudinary.com; script-src 'self' 'unsafe-inline' https://upload-widget.cloudinary.com https://maps.googleapis.com; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; frame-src 'self' https://upload-widget.cloudinary.com;",
   },
   {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains; preload",
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
   },
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
   },
   {
-    key: "X-Frame-Options",
-    value: "DENY",
+    key: 'X-Frame-Options',
+    value: 'DENY',
   },
   {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
   },
   {
-    key: "Referrer-Policy",
-    value: "no-referrer",
+    key: 'Referrer-Policy',
+    value: 'no-referrer',
   },
   {
-    key: "Permissions-Policy",
-    value: "geolocation=(), microphone=()",
+    key: 'Permissions-Policy',
+    value: 'geolocation=(), microphone=()',
   },
 ];
 
@@ -45,7 +36,7 @@ const nextConfig = {
     return [
       {
         // Apply these headers to all routes
-        source: "/(.*)",
+        source: '/(.*)',
         headers: securityHeaders,
       },
     ];
@@ -53,14 +44,14 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "media.gettyimages.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'media.gettyimages.com',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
       },
     ],
   },
